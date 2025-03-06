359a7ee9845fb563503f7d3ee686865a972f8ce4
Fernando Espidio <fer618@hotmail.com>
2025-03-06 12:39:50 -0600

    Backend with NoSQL Connection

--- controllers/login.controllers.js
+++ controllers/login.controllers.js
@@ -1,5 +1,6 @@
 import {sqlConnect, sql} from "../utils/sql.js"
 
+/*
 export const login = async (req, res) => {
     const pool = await sqlConnect();
 
@@ -15,7 +16,37 @@ export const login = async (req, res) => {
     
     let isLogin = data.recordset[0].password === req.body.password
 
-    res.status(200).json({isLogin: isLogin});
+    if(isLogin) {
+        res.status(400).json({isLogin: isLogin, user: data.recordset[0]});
+    }else {
+        res.status(400).json({isLogin: isLogin, user: {}});
+    }
+
+};
+*/
+
+export const login = async (req, res) => {
+    try {
+        const pool = await sqlConnect();
+        const data = await pool
+            .request()
+            .input("username", sql.VarChar, req.body.username)
+            .query("SELECT * FROM users WHERE username=@username");
+
+        if (data.recordset.length > 0) {
+            const isLogin = data.recordset[0].password === req.body.password;
+            if (isLogin) {
+                res.status(200).json({ isLogin: true, user: data.recordset[0] });
+            } else {
+                res.status(401).json({ isLogin: false, message: 'Incorrect password' });
+            }
+        } else {
+            res.status(404).json({ isLogin: false, message: 'User not found' });
+        }
+    } catch (error) {
+        console.error('Database connection or query error:', error);
+        res.status(500).json({ isLogin: false, message: 'Server error' });
+    }
 };
 
 export const signin = async (req, res) => {
--- index.js
+++ index.js
@@ -1,14 +1,25 @@
 import "dotenv/config"
+import cors from "cors";
 import express from "express"
 import indexRoutes from "./routes/index.routes.js"
 import itemsRoutes from "./routes/items.routes.js"
 import loginRoutes from "./routes/login.routes.js"
+import morgan from "morgan";
+import { connectDB } from "./utils/mongodb.js";
+import items2Routes from "./routes/items2.routes.js"
 
 const app = express();
 
+connectDB();
+
 app.use(express.json());
+
+app.use(cors());
+app.use(morgan("dev"));
+
 app.use(indexRoutes);
 app.use(itemsRoutes);
+app.use(items2Routes);
 app.use(loginRoutes);
 
 app.listen(5500, () => {
